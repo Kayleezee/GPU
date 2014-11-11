fe765b51fc6edc0a08dec2ed6ed9345ab3ff8ec5
gpucomp02 <gpucomp02@creek01.ziti.uni-heidelberg.de>
2014-11-11 21:09:01 +0100

    fixed part3.1

--- exercise_3/src/part_1/globalmemaccess.cu
+++ exercise_3/src/part_1/globalmemaccess.cu
@@ -1,6 +1,4 @@
-/*********************************************************************************
- * FILENAME         main.c
- * 
+ /* 
  * DESCRIPTION      These functions are part of the submission to exercises of 
  *                  the "GPU Computing" lecture of the University of Heidelberg.
  * 
@@ -21,30 +19,30 @@ int main( int argc, char *argv[])
 {
   int i;
   double dBandwidth;
-  void * dmem_a, dmem_b;     // device data (variable scope annotation)
-  int iIteration[10] = {10e3, 10e4, 10e5, 10e6, 5*10e6, 10e7, 5*10e7, 10e8, 5*10e8, 10e9};
+  void *dmem_a, *dmem_b;     // device data (variable scope annotation)
+  long iIteration[10] = {1e3, 1e4, 1e5, 1e6, 5e6, 1e7, 5e7, 1e8, 5e8, 1e9};
   chTimerTimestamp tsStart, tsStop;
   
   /* Run over 10 measurement points */
   for(i = 0; i < 10; i++)
   {
     /* Allocate device memory */
-    dmem_a = cudaMalloc(iIteration[i]*sizeof(char));
-    dmem_b = cudaMalloc(iIteration[i]*sizeof(char));
+    cudaMalloc(&dmem_a, iIteration[i]*sizeof(char));
+    cudaMalloc(&dmem_b, iIteration[i]*sizeof(char));
 
     /* Start timer */
     chTimerGetTime(&tsStart);
     /* Start memory copy */
-    cudaMemcpy(dmem_a ,dmem_b , iItaration[i]*sizeof(char), cudaMemcpyDeviceToDevice);
+    cudaMemcpy(dmem_a ,dmem_b , iIteration[i]*sizeof(char), cudaMemcpyDeviceToDevice);
     /* Stop timer */
     chTimerGetTime(&tsStop);
     
     /* Get bandwidth in Byte/sec and print it */
-    dBandwidth=(&tsStart, &tsStart, (double)iIteration[i] * sizeof(char));
-    printf("\n %d Byte : %lf B/s",iIteration[i],dBandwitdth);
+    dBandwidth=chTimerBandwidth(&tsStart, &tsStop, (double) iIteration[i] * sizeof(char));
+    printf(" %11li Byte : %.2e B/s\n",iIteration[i],dBandwidth);
     
     /* Free allocated device memory */
-    cudeFree(dmem_a);
+    cudaFree(dmem_a);
     cudaFree(dmem_b);
   }
 
