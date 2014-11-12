f32af6752f3a780bc361ab7f062d71fc347f82c1
gpucomp02 <gpucomp02@creek01.ziti.uni-heidelberg.de>
2014-11-12 21:44:38 +0100

    change part 3.1

--- exercise_3/src/part_1/globalmemaccess.cu
+++ exercise_3/src/part_1/globalmemaccess.cu
@@ -19,16 +19,23 @@ int main( int argc, char *argv[])
 {
   int i;
   double dBandwidth;
-  void *dmem_a, *dmem_b;     // device data (variable scope annotation)
-  long iIteration[10] = {1e3, 1e4, 1e5, 1e6, 5e6, 1e7, 5e7, 1e8, 5e8, 1e9};
+  char *dmem_a, *dmem_b;     // device data (variable scope annotation)
+  char *hmem;
+  long iIteration[13] = {1e3, 2e3, 1e4, 2e4, 1e5, 2e5, 1e6, 2e6, 1e7, 2e7, 1e8, 2e8, 5e9};
   chTimerTimestamp tsStart, tsStop;
   
-  /* Run over 10 measurement points */
-  for(i = 0; i < 10; i++)
+  /* Run over 13 measurement points */
+  for(i = 0; i < 13; i++)
   {
     /* Allocate device memory */
     cudaMalloc(&dmem_a, iIteration[i]*sizeof(char));
     cudaMalloc(&dmem_b, iIteration[i]*sizeof(char));
+    
+    hmem = (char*)malloc(iIteration[i]*sizeof(char));
+    /* Fill elements with '1' */
+    for(int j=0; j<iIteration[i]; j++)
+      hmem[j] = 'a';
+    cudaMemcpy(dmem_b, hmem, iIteration[i]*sizeof(char), cudaMemcpyHostToDevice);
 
     /* Start timer */
     chTimerGetTime(&tsStart);
@@ -38,9 +45,9 @@ int main( int argc, char *argv[])
     chTimerGetTime(&tsStop);
     
     /* Get bandwidth in Byte/sec and print it */
-    dBandwidth=chTimerBandwidth(&tsStart, &tsStop, (double) iIteration[i] * sizeof(char));
-    printf(" %11li Byte : %.2e B/s\n",iIteration[i],dBandwidth);
-    
+    dBandwidth=chTimerBandwidth(&tsStart, &tsStop, (double) iIteration[i]);
+    printf("%li %.2e\n",iIteration[i],dBandwidth/1e9);
+ 
     /* Free allocated device memory */
     cudaFree(dmem_a);
     cudaFree(dmem_b);
