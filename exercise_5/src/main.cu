474a80407a8502ed6fc2b0a98f79c487cbf80f91
gpucomp02 <gpucomp02@creek01.ziti.uni-heidelberg.de>
2014-12-03 12:39:49 +0100

    add source for exercise 5.3

--- exercise_5/src/main.cu
+++ exercise_5/src/main.cu
@@ -38,7 +38,6 @@ matMul_Kernel(int matrixSize, float* matrixA, float* matrixB, float* matrixC)
     int elementIdx = blockIdx.x * blockDim.x + threadIdx.x;
     int elementIdy = blockIdx.y * blockDim.y + threadIdx.y;
 
-    //int elementId = elementIdy * matrixSize + elementIdx;
     int elementId = elementIdx * matrixSize + elementIdy;
 
     if (elementIdx < matrixSize && elementIdy < matrixSize) {
@@ -61,18 +60,29 @@ __global__ void
 shMatMul_Kernel(int matrixSize, float* matrixA, float* matrixB, float* matrixC)
 {
     extern __shared__ float sh_Mem[];
-    //float sh_MatrixA = &(sh_Mem[0]);
-    //float sh_MatrixB = &(sh_Mem[1 + ]);
-    //float sh_MatrixC = &(sh_Mem[2 /*TODO Calc offset*/]);
+    int tilewidth = blockDim.x;
+    float *sh_MatrixA = &(sh_Mem[0]);
+    float *sh_MatrixB = &(sh_Mem[1*tilewidth*tilewidth]);
+    //float *sh_MatrixC= &(sh_Mem[2*tilewidth*tilewidth]);
 
-    //int elementIdx = blockIdx.x * blockDim.x + threadIdx.x;
-    //int elementIdy = blockIdx.y * blockDim.y + threadIdx.y;
+    int elementIdx = blockIdx.x * blockDim.x + threadIdx.x; // Col
+    int elementIdy = blockIdx.y * blockDim.y + threadIdx.y; // Row
 
-    //int elementId = elementIdy * matrixSize + elementIdx;
-
-    //if (elementIdx < matrixSize && elementIdy < matrixSize) {
-        /*TODO Kernel Code*/
-    //}
+    int elementId = elementIdy * matrixSize + elementIdx;
+    float CValue = 0;
+    if (elementIdx < matrixSize && elementIdy < matrixSize) {
+        for(int m=0; m < (matrixSize/tilewidth); ++m)
+        {
+          sh_MatrixA[tilewidth*threadIdx.y + threadIdx.x] = matrixA[elementIdy*matrixSize + (m*tilewidth+threadIdx.x)];
+          sh_MatrixB[tilewidth*threadIdx.y + threadIdx.x] = matrixB[elementIdx + (m*tilewidth+threadIdx.y)*matrixSize];
+          __syncthreads();
+ 
+          for(int k=0; k<tilewidth; ++k)
+            CValue += sh_MatrixA[tilewidth*threadIdx.y + k] * sh_MatrixB[tilewidth*k + threadIdx.x];
+          __syncthreads();
+        }
+      matrixC[elementId] = CValue; 
+    }
 }
 //
 // Main
@@ -212,9 +222,8 @@ main(int argc, char * argv[])
                       << std::endl
               << "***" << std::endl;
 
-    // TODO Calc shared mem size
-    int sharedMemSize = 3 * blockSize * blockSize * sizeof(float);
-
+    int sharedMemSize = 2 * blockSize * blockSize * sizeof(float);
+    
     kernelTimer.start();
 
     //
