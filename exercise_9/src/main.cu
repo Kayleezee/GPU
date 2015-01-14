a8d9eddedc485009a0a4900deb2c7237178abed8
gpucomp02 <gpucomp02@creek01.ziti.uni-heidelberg.de>
2015-01-14 17:27:37 +0100

    update ex9

--- exercise_9/src/main.cu
+++ exercise_9/src/main.cu
@@ -20,9 +20,11 @@
 #include <cstdio>
 #include <iomanip>
 
+#define _OPT_KERNEL
 const static int DEFAULT_NUM_ELEMENTS   = 1024;
 const static int DEFAULT_NUM_ITERATIONS =    5;
 const static int DEFAULT_BLOCK_DIM      =  128;
+const static int DEFAULT_STEPS          =  100;
 
 //
 // Structures
@@ -51,58 +53,73 @@ isBorder(int iSize, int iIndex)
 
 
 __global__ void
-simpleStencil_Kernel(StencilArray_t d_array)
+simpleStencil_Kernel(StencilArray_t d_array_old, StencilArray_t d_array_new)
 {
     // index of array
     int iIndex = blockIdx.x * blockDim.x + threadIdx.x;
 
     // value of actual element
-    float fThisValue = d_array.array[iIndex];
+    float fThisValue = d_array_old.array[iIndex];
 
     float act_sum;
 
-    if(!isBorder(d_array.size, iIndex)) {
+    for(int i=0; i<DEFAULT_STEPS; i++){
+      if(!isBorder(d_array_old.size, iIndex)) {
 
         act_sum = fThisValue + 24.0 / 100.0
                   * ((-4.0) * fThisValue
-                  + d_array.array[iIndex + 1]
-                  + d_array.array[iIndex - 1]
-                  + d_array.array[iIndex + d_array.size]
-                  + d_array.array[iIndex - d_array.size]);
-
-        d_array.array[iIndex] = act_sum;
+                  + d_array_old.array[iIndex + 1]
+                  + d_array_old.array[iIndex - 1]
+                  + d_array_old.array[iIndex + d_array_old.size]
+                  + d_array_old.array[iIndex - d_array_old.size]);
+      }
     }
-
+    d_array_new.array[iIndex] = act_sum;
     //__syncthreads();
     //d_array.array[iIndex] = act_sum;
 }
 
 __global__ void
-optStencil_Kernel(StencilArray_t d_array)
+optStencil_Kernel(StencilArray_t d_array_old, StencilArray_t d_array_new)
 {
     extern __shared__ float shMemBlock[];
 
     int iIndex = blockIdx.x * blockDim.x + threadIdx.x;
+    int size = d_array_old.size;
+    int iOffset = size;
 
-    float fThisValue = d_array.array[iIndex];
+    float fThisValue = d_array_old.array[iIndex];
 
-    shMemBlock[threadIdx.x] = fThisValue;
-
-    __syncthreads();
+    shMemBlock[threadIdx.x+iOffset] = fThisValue;
 
     float act_sum;
 
-    if(!isBorder(d_array.size, iIndex)) {
+    for(int i=0; i<DEFAULT_STEPS; i++){
+      /* Copy the helo-Values to shared memory */
+      if(threadIdx.x < size && iIndex >= size)
+        shMemBlock[threadIdx.x] = d_array_old.array[iIndex-size];
+      if((threadIdx.x >= blockDim.x - size) && (iIndex < (blockDim.x*gridDim.x - size)))
+        shMemBlock[threadIdx.x+2*iOffset] = d_array_old.array[iIndex+size];
+
+      __syncthreads();
+
+      if(!isBorder(size, iIndex)) {
 
         act_sum = fThisValue + 24.0 / 100.0
                   * ((-4.0) * fThisValue
-                  + shMemBlock[threadIdx.x + 1]
-                  + shMemBlock[threadIdx.x - 1]
-                  + shMemBlock[threadIdx.x + d_array.size]
-                  + shMemBlock[threadIdx.x - d_array.size]);
+                  + shMemBlock[threadIdx.x + iOffset + 1]
+                  + shMemBlock[threadIdx.x + iOffset - 1]
+                  + shMemBlock[threadIdx.x + iOffset + size]
+                  + shMemBlock[threadIdx.x + iOffset - size]);
+      }
 
-        d_array.array[iIndex] = act_sum;
+      /* Copy back the helo-Values to global memory */
+      if(threadIdx.x < size && iIndex >= size)
+        d_array_old.array[iIndex-size] = shMemBlock[threadIdx.x];
+      if((threadIdx.x >= blockDim.x - size) && (iIndex < (blockDim.x*gridDim.x - size)))
+        d_array_old.array[iIndex+size] = shMemBlock[threadIdx.x+2*iOffset];
     }
+    d_array_new.array[iIndex] = act_sum;
 }
 //
 // Main
@@ -125,7 +142,7 @@ main(int argc, char * argv[])
               << "***" << std::endl;
 
     ChTimer memCpyH2DTimer, memCpyD2HTimer;
-    ChTimer kernelTimer;
+    ChTimer kernelTimer, kernelTimer_simple;
 
     /*
     ofstream fout;
@@ -178,11 +195,15 @@ main(int argc, char * argv[])
     std::cout << "Step 2" << std::endl;
 
     // Device Memory
-    StencilArray_t d_array;
-    cudaMalloc(&(d_array.array),
+    int iIndex = 0;
+    StencilArray_t d_array[2];
+    cudaMalloc(&(d_array[0].array),
             static_cast<size_t>(numElements * sizeof(*h_array.array)));
 
-    if (h_array.array == NULL || d_array.array == NULL) {
+    cudaMalloc(&(d_array[1].array),
+            static_cast<size_t>(numElements * sizeof(*h_array.array)));
+
+    if (h_array.array == NULL || d_array[0].array == NULL || d_array[1].array == NULL) {
         std::cout << "\033[31m***" << std::endl
                   << "*** Error - Memory allocation failed" << std::endl
                   << "***\033[0m" << std::endl;
@@ -197,11 +218,12 @@ main(int argc, char * argv[])
     //
     memCpyH2DTimer.start();
 
-    cudaMemcpy(d_array.array, h_array.array,
-            static_cast<size_t>(numElements * sizeof(*d_array.array)),
+    cudaMemcpy(d_array[iIndex].array, h_array.array,
+            static_cast<size_t>(numElements * sizeof(*d_array[iIndex].array)),
             cudaMemcpyHostToDevice);
 
-    d_array.size = h_array.size;
+    d_array[0].size = h_array.size;
+    d_array[1].size = h_array.size;
 
     memCpyH2DTimer.stop();
 
@@ -231,7 +253,8 @@ main(int argc, char * argv[])
         exit(-1);
     }
 
-    gridSize = ceil(static_cast<float>(d_array.size) / static_cast<float>(blockSize));
+    //gridSize = ceil(static_cast<float>(d_array[iIndex].size) / static_cast<float>(blockSize));
+    gridSize = ceil(numElements / blockSize)+1;
 
     dim3 grid_dim = dim3(gridSize);
     dim3 block_dim = dim3(blockSize);
@@ -241,20 +264,18 @@ main(int argc, char * argv[])
     std::cout << "*** Block: " << blockSize << std::endl;
     std::cout << "***" << std::endl;
 
-    float shMemSize = blockSize * sizeof(float);
+    float shMemSize = (blockSize + 2 * d_array[iIndex].size) * sizeof(float);
 
     kernelTimer.start();
 
     for (int i = 0; i < numIterations; i ++) {
-#ifndef _OPT_KERNEL
-		simpleStencil_Kernel<<<grid_dim, block_dim>>>(d_array);
-#else
-        optStencil_Kernel<<<grid_dim, block_dim, shMemSize>>>(d_array);
-#endif
+      optStencil_Kernel<<<grid_dim, block_dim, shMemSize>>>(d_array[iIndex], d_array[1-iIndex]);
+      cudaDeviceSynchronize();
+      iIndex = 1 - iIndex;
     }
 
     // Synchronize
-    cudaDeviceSynchronize();
+    //cudaDeviceSynchronize();
 
     // Check for Errors
     cudaError_t cudaError = cudaGetLastError();
@@ -269,12 +290,36 @@ main(int argc, char * argv[])
 
     kernelTimer.stop();
 
+    kernelTimer_simple.start();
+
+    for (int i = 0; i < numIterations; i ++) {
+      simpleStencil_Kernel<<<grid_dim, block_dim>>>(d_array[iIndex], d_array[1-iIndex]);
+      cudaDeviceSynchronize();
+      iIndex = 1 - iIndex;
+    }
+
+    // Synchronize
+    //cudaDeviceSynchronize();
+
+    // Check for Errors
+    cudaError_t cudaError2 = cudaGetLastError();
+    if ( cudaError2 != cudaSuccess ) {
+        std::cout << "\033[31m***" << std::endl
+                  << "***ERROR*** " << cudaError2 << " - " << cudaGetErrorString(cudaError2)
+                  << std::endl
+                  << "***\033[0m" << std::endl;
+
+        return -1;
+    }
+
+    kernelTimer_simple.stop();
+
     //
     // Copy Back Data
     //
     memCpyD2HTimer.start();
 
-    cudaMemcpy(h_array.array, d_array.array,
+    cudaMemcpy(h_array.array, d_array[1-iIndex].array,
             static_cast<size_t>(h_array.size * sizeof(*(h_array.array))),
             cudaMemcpyDeviceToHost);
 
@@ -293,7 +338,8 @@ main(int argc, char * argv[])
         cudaFreeHost(h_array.array);
     }
 
-    cudaFree(d_array.array);
+    cudaFree(d_array[0].array);
+    cudaFree(d_array[1].array);
 
     // Print Meassurement Results
     std::cout << "***" << std::endl
@@ -310,7 +356,9 @@ main(int argc, char * argv[])
               << "***    Copy Bandwidth: "
                 << 1e-9 * memCpyD2HTimer.getBandwidth(numElements * sizeof(*h_array.array))
                 << " GB/s" << std::endl
-              << "***    Time for Stencil Computation: " << 1e3 * kernelTimer.getTime()
+              << "***    Time for Stencil Computation (optimized): " << 1e3 * kernelTimer.getTime() / numIterations
+                << " ms" << std::endl
+              << "***    Time for Stencil Computation (simple): " << 1e3 * kernelTimer_simple.getTime() / numIterations
                 << " ms" << std::endl
               << "***" << std::endl;
 
